b04f04e340cd3d2481b174fa3244fa51b1476745
jadanza1 <43007037+jadanza1@users.noreply.github.com>
2019-01-08 09:35:29 -0700

    Not finished Earned MArks

--- src/CSharp.Language.Review/EarnedMark.cs
+++ src/CSharp.Language.Review/EarnedMark.cs
@@ -6,7 +6,37 @@ using System.Threading.Tasks;
 
 namespace CSharp.Language.Review
 {
-    class EarnedMark
+    public class EarnedMark : WeightedMark
     {
+        public int Possible { get; private set; }
+        private double _Earned;
+        public double Earned
+        {
+            get { return Earned; }
+            set
+            {
+                if (value < 0 || value > Possible)
+                    throw new Exception("Invalid earned mark assigned");
+                _Earned = value;
+            }
+        }
+        public double Percent
+        {  get { return(Earned / Possible) * 100; } }
+        
+        public double WeightedPercent
+        { get { return Percent * Weight / 100; } }
+
+        public EarnedMark(string name, int weight , int possible, double earned) : base(name, weight)
+        {
+            if (possible <= 0)
+                throw new Exception("Invalid Possible Marks");
+            Possible = possible;
+            Earned = earned;
+        }
+
+        //public override string ToString()
+        //{
+        //    return string.Format("{0} ({1})\t - {2}% ({3}/{4}) \ t - W")
+        //}
     }
 }
